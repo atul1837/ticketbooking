dbcc18df087c16864da65168397ac77ae4f8d72f
Atul Thakur <atul.thakur1837@gmail.com>
2020-04-25 16:14:33 +0530

    login and signup integrated in main.cpp

--- User.h
+++ User.h
@@ -1,5 +1,6 @@
 #include "iostream"
 #include "fstream"
+//#include "extra.h"
 using namespace std;
 
 class User {
@@ -20,51 +21,43 @@ public:
 		UserName = name;
 	}
 
-	void signUp() {
+	void signUp(string name, string pass) {
 		fstream fo;
 		fstream fo2;
 		string line;
 		int c = 0;
-		cout << "Enter Username: ";
 		fo.open("Username.txt", ios::in|ios::out|ios::app);
-		getline(cin,line);
-		fo << line << endl;
+		fo << name << endl;
 		fo.seekg(0, ios::beg);
-		addUserName(line); 
+		addUserName(name); 
 		while(fo) {
 			getline(fo,line);
 			c = c + 1;
 		}
 		fo2.open("Userpassword.txt", ios::in|ios::out|ios::app);
-		cout << "Enter Password: ";
-		getline(cin,line);
-		fo2 << line << endl;
+		fo2 << pass << endl;
 		addUserId(c-1);
-		cout << "Your UserId: " << c-1 << endl;	
+		cout << "Never forgett your userId you will not be able to use your account without userId" << endl;
+		cout << "Your UserId: " << c-1 << endl;
+		cout << "Enter 0 to continue : ";		
+		cin >> line;
 		fo.close();
 		fo2.close();	
 	}
 
-	bool login() {
+	bool login(string pass, int n) {
 		fstream fo;
 		fstream fo2;
 		string line1;
 		string line2;
-		string pass;
-		int n;
-		cout << "Enter UserId: ";
-		cin >> n;
 		fo.open("Userpassword.txt", ios::in|ios::out|ios::app);
 		for(int i = 0; i < n; i++) {
 			getline(fo, line1);
 		}
 		fo2.open("Username.txt", ios::in|ios::out|ios::app);
 		for(int i = 0; i < n; i++) {
-			getline(fo,line2);
+			getline(fo2,line2);
 		}
-		cout << "Enter Password: ";
-		cin >> pass;
-
 		if(pass == line1) {
 			addUserId(n);
 			addUserName(line2);
--- main.cpp
+++ main.cpp
@@ -1,17 +1,62 @@
 #include "iostream"
 #include "User.h"
 #include "Movie.h"
+#include "extra.h"
 using namespace std;
 
 
 int main() {
 	User a;
 	Movie m;
-	//a.signUp();
-	//cout << a.getUserId();
-	//bool b = a.login();
-	//cout << b;
-	m.addMovie();
-	m.getMovies();
+	string s;
+	while(true) {
+		system("clear");
+		printHLine();
+		printstarline();
+		cout << "WELCOME to PVR Ticket Booking software" <<endl;
+		if(a.isLoggedIn()) {
+			string name = a.getUserName();
+			cout << "WELCOME " << name << endl;
+		}
+		cout << "Enter quit if you want to exit" << endl;
+		printstarline();
+		printHLine();
+		if(!a.isLoggedIn()) {
+			cout << "Do you have an account ? (Y/N)" << endl;
+			cin >> s;
+			quit(s);
+			if(s == "Y" || s == "y") {
+				int n;
+				string pass;
+				cout << "Enter UserId: ";
+				cin >> n;
+				cout << "Enter Password: ";
+				cin >> pass;
+				quit(pass);
+				a.login(pass, n);
+			}
+			if(s == "N" || s == "n") {
+				string name;
+				string pass;
+				cout << "Enter UserName: ";
+				cin >> name;
+				quit(name);
+				cout << "Enter Password: ";
+				cin >> pass;
+				quit(pass);
+				a.signUp(name, pass);
+			}
+		} else {
+			if(a.isLoggedIn()) {
+				cout << "What would you like to do ??" <<endl;
+				printHLine();
+				cout << "1. Book Ticket for Movie" << endl;
+				cout << "2. Add new Movie to database" << endl; 
+				cout << "3. Check which Movie Tickets you already booked" << endl;
+				cin >> s;
+				quit(s);
+			}
+		}
+	}
 	return 0;
 }
